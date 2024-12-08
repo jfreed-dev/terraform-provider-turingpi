cfb7a9fef7d23d572963bf1e0665adac423d4e81
Jonathan Freed <jon@freed.dev>
2024-12-08 18:09:39 -0500

    updating provider

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"turingpi/provider"
+	"github.com/jfreed-dev/turingpi-terraform-provider/provider"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
