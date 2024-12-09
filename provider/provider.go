30a75b0ad0cedf106b78ee4e5888863b2709380b
Jonathan Freed <jon@freed.dev>
2024-12-09 10:03:33 -0500

    - all resourceFlash-related logic is moved to resource_flash.go - Remove any duplicate definitions from provider.go

--- provider/provider.go
+++ provider/provider.go
@@ -1,54 +1,27 @@
 package provider
 
 import (
-	"fmt"
-
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-func resourceFlash() *schema.Resource {
-	return &schema.Resource{
-		Create: resourceFlashCreate,
-		Read:   resourceFlashRead,
-		Delete: resourceFlashDelete,
-		// No Update function is defined, so ForceNew is required for fields that trigger recreation
+func Provider() *schema.Provider {
+	return &schema.Provider{
 		Schema: map[string]*schema.Schema{
-			"node": {
-				Type:        schema.TypeInt,
-				Required:    true,
-				Description: "Node ID to flash firmware",
-				ForceNew:    true, // Force resource recreation if this changes
-			},
-			"firmware_file": {
+			"username": {
 				Type:        schema.TypeString,
 				Required:    true,
-				Description: "Path to the firmware file",
-				ForceNew:    true, // Force resource recreation if this changes
+				Description: "The username for BMC authentication",
+			},
+			"password": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "The password for BMC authentication",
 			},
 		},
+		ResourcesMap: map[string]*schema.Resource{
+			"turingpi_power": resourcePower(),
+			"turingpi_flash": resourceFlash(),
+			"turingpi_node":  resourceNode(),
+		},
 	}
-}
-
-func resourceFlashCreate(d *schema.ResourceData, meta interface{}) error {
-	node := d.Get("node").(int)
-	firmware := d.Get("firmware_file").(string)
-
-	// Example logic for flashing
-	fmt.Printf("Flashing node %d with firmware %s\n", node, firmware)
-
-	// Set a unique ID for the resource
-	d.SetId(fmt.Sprintf("node-%d", node))
-	return nil
-}
-
-func resourceFlashRead(d *schema.ResourceData, meta interface{}) error {
-	// Example logic for reading flash status
-	fmt.Printf("Reading flash status for node %s\n", d.Id())
-	return nil
-}
-
-func resourceFlashDelete(d *schema.ResourceData, meta interface{}) error {
-	// Example logic for flash cleanup if needed
-	fmt.Printf("Deleting flash resource for node %s\n", d.Id())
-	return nil
 }
