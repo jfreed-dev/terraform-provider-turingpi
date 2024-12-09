de0aa102f1b5647521831db4c8f9b5fa3df58cbc
Jonathan Freed <jon@freed.dev>
2024-12-09 10:13:12 -0500

    Update resource_flash.go

--- provider/resource_flash.go
+++ provider/resource_flash.go
@@ -11,18 +11,19 @@ func resourceFlash() *schema.Resource {
 		Create: resourceFlashCreate,
 		Read:   resourceFlashRead,
 		Delete: resourceFlashDelete,
+		// No Update function provided, so ForceNew is mandatory for all mutable fields
 		Schema: map[string]*schema.Schema{
 			"node": {
 				Type:        schema.TypeInt,
 				Required:    true,
 				Description: "Node ID to flash firmware",
-				ForceNew:    true,
+				ForceNew:    true, // Mark this field as requiring recreation
 			},
 			"firmware_file": {
 				Type:        schema.TypeString,
 				Required:    true,
 				Description: "Path to the firmware file",
-				ForceNew:    true,
+				ForceNew:    true, // Mark this field as requiring recreation
 			},
 		},
 	}
@@ -32,17 +33,22 @@ func resourceFlashCreate(d *schema.ResourceData, meta interface{}) error {
 	node := d.Get("node").(int)
 	firmware := d.Get("firmware_file").(string)
 
+	// Example flashing logic
 	fmt.Printf("Flashing node %d with firmware %s\n", node, firmware)
+
+	// Set the unique resource ID
 	d.SetId(fmt.Sprintf("node-%d", node))
 	return nil
 }
 
 func resourceFlashRead(d *schema.ResourceData, meta interface{}) error {
+	// Example logic for reading flash status
 	fmt.Printf("Reading flash status for node %s\n", d.Id())
 	return nil
 }
 
 func resourceFlashDelete(d *schema.ResourceData, meta interface{}) error {
+	// Example logic for cleaning up a flashed node
 	fmt.Printf("Deleting flash resource for node %s\n", d.Id())
 	return nil
 }
